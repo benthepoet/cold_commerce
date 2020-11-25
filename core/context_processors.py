3d33b9179f90eef75109b077215d29e2a96106d8
Ben Hanna <benpaulhanna@gmail.com>
2020-11-25 10:48:39 -0500

    Added cart display.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,16 @@
-from .models import Category
+from .models import Category, CartLine
+
+def cart(request):
+    cart = request.session.session_key
+
+    if cart is None:
+        cart_count = 0
+    else:
+        cart_count = CartLine.objects.filter(cart=cart).count()
+
+    return {
+        'cart_count': cart_count
+        }
 
 def categories(request):
     return {
